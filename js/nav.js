e827840b11571f06112573a70ba2c73e17ea908a
matluc <mlucarz1@gmail.com>
2018-02-21 16:19:20 +0100

    Modal

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,13 @@
-const navToggle = document.querySelector('.menu-button')
-const nav = document.querySelector('nav')
+const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+const containerAll = document.querySelector('.container-all');
 
 navToggle.addEventListener('click', _ => {
-  document.body.classList.toggle('nav-is-open')
+  containerAll.style.transition = 'transform 250ms ease-in-out';
+  document.body.classList.toggle('nav-is-open');
 })
 
 nav.addEventListener('click', _ => {
-  document.body.classList.remove('nav-is-open')
+  containerAll.style.transition = '0ms';
+  document.body.classList.remove('nav-is-open');
 })
